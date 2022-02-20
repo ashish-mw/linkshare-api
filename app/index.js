474a0dff8ae0b7b54f64edbb9c7a3414be677cab
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2022-02-21 00:24:06 +0530

    add cors

--- app/index.js
+++ app/index.js
@@ -4,10 +4,12 @@ const config = require("./config");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 const routes = require("./routes");
 
 const { notFound, errorHandler, deserializeUser } = require("./middlewares");
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("combined"));
 app.use(deserializeUser);
